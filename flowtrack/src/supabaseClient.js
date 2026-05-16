0078547e4068102cb9b25e9f640df024d0d52279
MMujaddid <m.mujaddid2003@gmail.com>
2026-05-16 23:06:38 +0500

    fix: updated to valid eyJ token

--- flowtrack/src/supabaseClient.js
+++ flowtrack/src/supabaseClient.js
@@ -2,7 +2,7 @@ import { createClient } from '@supabase/supabase-js';
 
 // Hardcode your actual credentials directly into the client setup
 const supabaseUrl = 'https://qthgnuefbnmzwgxxvrfi.supabase.co';
-const supabaseAnonKey = 'sb_publishable_FBvMYl6OhjokZub93tpNTA_FK6HI-Yp';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF0aGdudWVmYm5tendneHh2cmZpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM2NTk1MjMsImV4cCI6MjA4OTIzNTUyM30.TxgYlqsH65Fwl7umpUmwbsGJUpJyKhGfr1bomDR3RE0';
 
 if (!supabaseUrl || !supabaseAnonKey) {
   console.error("Missing Supabase environment variables!");
